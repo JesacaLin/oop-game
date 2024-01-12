5cbbba115e179612a3187233bd99f5dd3ff56bbd
Qiao Juan Lin <JesacaLin@gmail.com>
2024-01-11 19:33:32 -0500

    Player class set. Initialized Game class

---
+++ src/main/java/org/JesacaLin/Game.java
@@ -0,0 +1,4 @@
+package org.JesacaLin;
+
+public class Game {
+}
---
+++ src/main/java/org/JesacaLin/Player.java
@@ -0,0 +1,32 @@
+package org.JesacaLin;
+
+import java.util.Random;
+
+public class Player {
+    private String name;
+    private String choice;
+    private boolean isComputer;
+    private static final String[] CHOICES = {"rock", "paper", "scissors"};
+
+    public Player (String name, String choice) {
+        this.name = name;
+        this.choice = choice;
+    }
+
+    public String getPlayer() {
+        return this.name;
+    }
+    public String getChoice() {
+        return this.choice;
+    }
+    public void makeChoice() {
+        if (isComputer) {
+            Random random = new Random();
+            this.choice = CHOICES[random.nextInt(CHOICES.length)];
+        } else {
+            this.choice = choice;
+        }
+
+    }
+
+}
